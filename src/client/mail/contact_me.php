521f60b3bc5dd16b74295f4983221b0d01b6e717
Brenton Cozby <brenton.cozby@gmail.com>
2017-04-27 19:21:11 -0400

    fix mobile technology thing

--- src/client/mail/contact_me.php
+++ src/client/mail/contact_me.php
@@ -21,7 +21,7 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create the email and send the message
-$to = 'service@rallypointwebinars.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'ajoslow@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: contact@rallypointwebinars.com\r\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
@@ -29,6 +29,6 @@ $headers .= "Reply-To: $email_address\r\n";
 $headers .= "MIME-Version: 1.0\r\n";
 $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 $headers .= "X-Priority: 1\r\n";
-mail($to,$email_subject,$email_body,$headers);
-return true;
+$status = mail($to,$email_subject,$email_body,$headers);
+return $status;
 ?>
